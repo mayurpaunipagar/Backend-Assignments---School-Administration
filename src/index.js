028e7d82cbcd1bc3fc769725cb0b86eb858373a4
MayurHpNotebook <001mayur94@gmail.com>
2020-12-05 10:44:07 +0530

    added get request functionality

--- src/index.js
+++ src/index.js
@@ -1,16 +1,49 @@
 const express = require('express')
 const app = express()
-const bodyParser = require("body-parser");
-const port = 8080
-app.use(express.urlencoded());
+app.use(express.json());
+const studentArray = require('./InitialData.js');
+
+        //const bodyParser = require("body-parser");
+const port = 8081;
+        //app.use(express.urlencoded());
 
 // Parse JSON bodies (as sent by API clients)
-app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+
+        //app.use(bodyParser.urlencoded({ extended: false }))
+        //app.use(bodyParser.json())
 // your code goes here
 
 
+
+app.get('/api/student',(req,res)=>{
+    res.send(studentArray);
+});
+app.get('/api/student/:id',(req, res)=>{
+    if(req.params.id>studentArray.length || req.params.id<0){
+        res.sendStatus(404);    
+    }else{
+        res.send(studentArray[req.params.id-1]);
+    }
+})
+// app.post('/api/student',(req,res)=>{
+//     res.set({'content-type':'application/x-www-form-urlencoded'});
+//     // if(req.body.name!=="" || req.body.currentClass ){
+
+//     // }
+//     const id=studentArray.length+1;
+//     const name=req.body.name;
+//     const currentClass=req.body.currentClass;
+//     const division=req.body.division;
+
+//     studentArray.push({
+//         "id":id,
+//         "name":name,
+//         "currentClass":currentClass,
+//         "division":division
+//     });
+//     res.send(studentArray);
+//     console.log(studentArray);
+// });
 app.listen(port, () => console.log(`App listening on port ${port}!`))
 
 module.exports = app;   
